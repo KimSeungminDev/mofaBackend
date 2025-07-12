3c7539ebda3ab808b718c28da83cfa469f4289a3
KimSeungminDev <kimseungmin.dev@gmail.com>
2025-07-12 14:18:48 +0900

    리액트 라우터용 config

---
+++ src/main/java/com/springboot/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // React 라우터가 처리할 수 있게 모든 경로를 index.html로 포워딩
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    }
+}
