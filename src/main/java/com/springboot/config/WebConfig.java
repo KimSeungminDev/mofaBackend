94e9d37600b970a646f87ea9d30062586884c97f
KimSeungminDev <kimseungmin.dev@gmail.com>
2025-07-14 00:44:31 +0900

    CORS 설정시도 재시도

--- src/main/java/com/springboot/config/WebConfig.java
+++ src/main/java/com/springboot/config/WebConfig.java
@@ -14,11 +14,12 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://abroad.kozow.com",
-                                "http://43.202.4.30"
-                        ) // 허용할 프론트엔드 도메인
+                                "*"
+                        ) // 허용할 프론트엔드 도메인 (모든 도매안)
                         .allowedMethods("*") // GET, POST 등 모두 허용
-                        .allowedHeaders("*"); // 모든 헤더 허용
+                        .allowedHeaders("*") // 모든 헤더 허용
+                        .allowCredentials(false)
+                        .maxAge(3600);
             }
         };
     }
